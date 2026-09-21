d3f5dd3dad6d1aee8912d9cfb96394cf49883a68
ryansutc <ryansutc@gmail.com>
2026-09-20 20:11:23 -0700

    Add wrangler for cloudfare deployment and fixup .env variables for deployment

--- frontend/src/utils/environment.ts
+++ frontend/src/utils/environment.ts
@@ -12,13 +12,14 @@
  */
 export const BACKEND_DOMAIN: string =
   import.meta.env.VITE_BACKEND_DOMAIN || "http://localhost:8000/";
-export const ACCOUNT_URI: string = import.meta.env.VITE_ACCOUNT_URI;
 export const API_TIMEOUT = parseInt(
   import.meta.env.VITE_API_TIMEOUT || "5000",
-  10
+  10,
 ); // Default to 5000ms if not set
 export const API_TOKEN = localStorage.getItem("token") || null;
-export const GOOGLE_LOGIN_URL = import.meta.env.VITE_GOOGLE_LOGIN_URL || "http://localhost:8000/api/auth/google/";
+export const GOOGLE_LOGIN_URL =
+  import.meta.env.VITE_GOOGLE_LOGIN_URL ||
+  "http://localhost:8000/api/auth/google/";
 
 const viteEnv = import.meta.env.VITE_ENV ?? import.meta.env.MODE;
 export const IS_PRODUCTION = viteEnv === "production";
